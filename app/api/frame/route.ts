4838ee101189621d71c944d8c760bcc3452c81a8
Xharles <charlesnzubechukwu000@gmail.com>
2024-02-11 21:48:09 +0100

    re-route after 10 items

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -1,18 +1,24 @@
-import { FrameRequest, getFrameMessage, getFrameHtmlResponse } from '@coinbase/onchainkit';
-import { NextRequest, NextResponse } from 'next/server';
+import {
+  FrameRequest,
+  getFrameMessage,
+  getFrameHtmlResponse,
+} from "@coinbase/onchainkit";
+import { NextRequest, NextResponse } from "next/server";
 
 async function getResponse(req: NextRequest): Promise<NextResponse> {
-  let accountAddress: string | undefined = '';
-  let text: string | undefined = '';
+  let accountAddress: string | undefined = "";
+  let text: string | undefined = "";
 
   const searchParams = req.nextUrl.searchParams;
-  const id: any = searchParams.get('id');
+  const id: any = searchParams.get("id");
   const idAsNumber = parseInt(id);
 
   const nextId = idAsNumber + 1;
 
   const body: FrameRequest = await req.json();
-  const { isValid, message } = await getFrameMessage(body, { neynarApiKey: 'NEYNAR_ONCHAIN_KIT' });
+  const { isValid, message } = await getFrameMessage(body, {
+    neynarApiKey: "NEYNAR_ONCHAIN_KIT",
+  });
 
   if (isValid) {
     accountAddress = message.interactor.verified_accounts[0];
@@ -30,13 +36,16 @@ async function getResponse(req: NextRequest): Promise<NextResponse> {
     getFrameHtmlResponse({
       buttons: [
         {
-          label: id=== 10? `More news at Alphaday.com`: `Next`,
+          label: id === 10 ? `More news at Alphaday.com` : `Next`,
         },
       ],
       image: {
         src: `${process.env.NEXT_PUBLIC_BASE_URL}/api/gen-images?id=${id}`,
       },
-      postUrl: `${process.env.NEXT_PUBLIC_BASE_URL}/api/frame?id=${nextId}`,
+      postUrl:
+        id === 10
+          ? "https://app.alphaday.com/"
+          : `${process.env.NEXT_PUBLIC_BASE_URL}/api/frame?id=${nextId}`,
     })
   );
 }
@@ -45,4 +54,4 @@ export async function POST(req: NextRequest): Promise<Response> {
   return getResponse(req);
 }
 
-export const dynamic = 'force-dynamic';
+export const dynamic = "force-dynamic";
